f60e9d806770fe6a82bfb8869bde93eb7adef7dc
Dino Horvat <dxh3401@rit.edu>
2018-09-26 14:50:05 +0200

    Finalized

--- src/app/components/line-builder/line-builder.component.ts
+++ src/app/components/line-builder/line-builder.component.ts
@@ -11,23 +11,37 @@ declare var joint: any;
 })
 
 export class LineBuilderComponent implements OnInit {
+  graphs = [];
   graph: any = new joint.dia.Graph;
   paper: any;
   paperScroller: any;
   link: any;
   element: any;
   nodes: any;
-  code;
+  code = "AQW#@45L100@-90L50#";
 
-  cod = "AQW#@0L100@90L50@90L50#";
+  formas = [];
+
 
   split;
   ngOnInit(){
-    let splitCode = this.cod.split('@');
-    splitCode.shift();
-    splitCode[splitCode.length - 1] = splitCode[splitCode.length -1 ].replace('#','');
-    this.split = splitCode;
-    this.initDiagram();
+    this.formas =[
+      "AQW#@0L100@90L50#",
+      "AQW#@45L100#",
+      "AQW#@45L100@-90L50#",
+      "AQW#@0L100@90L50@90L50#",
+      "AQW#@0L100@90L50@90L50@90L150#",
+      "AQW#@45L100@-90L100@90L100@-90L100@90L100@-90L100@90L100#",
+      "AQW#@0L500@35L500@-120L500#",
+      "AQW#@0L500@35L500@-179L500#",
+      "esto no es una forma válida",
+      "AQW#@0L50@35L50@185L50#",
+      "AQW#@0L500@35L500#@-120L500#"
+    ];
+    for(let forma of this.formas){
+      this.initDiagram(forma);
+    }
+
     var x1 = 100;
     var y1 = 0;
     var x2 = x1 + 50 * Math.cos(90 * (180 / Math.PI));
@@ -48,12 +62,12 @@ export class LineBuilderComponent implements OnInit {
     console.log('-----------------');
 
   }
-  initDiagram() {
+  initDiagram(code) {
      this.graph = new joint.dia.Graph();
      this.paper = new joint.dia.Paper({
       model: this.graph,
       width: 600,
-      height: 300,
+      height: 400,
       gridSize: 1,
       defaultLink: new joint.shapes.org.Arrow()
     });
@@ -62,36 +76,52 @@ export class LineBuilderComponent implements OnInit {
       paper: this.paper,
       autoResizePaper: true
     });
+
+    paperScroller.$el.css({ width: '100%', height: '100%' }).appendTo('#paper-container');
+    paperScroller.zoom(-0.2);
+    paperScroller.centerContent();
+    this.addCell(this.graph, code);
+    let obj = {
+      graph: this.graph,
+      code: code
+    };
+    this.graphs.push(obj);
+    console.log(this.graphs);
+  }
+  addCell(graph, code){
+    console.log(graph);
+    graph.clear();
+    let splitCode = code.split('@');
+    splitCode.shift();
+    splitCode[splitCode.length - 1] = splitCode[splitCode.length -1 ].replace('#','');
+    this.split = splitCode;
+    console.log(this.split);
+    let prevAngle = 0;
+    let prevLength = 0;
+    let preY = 0;
     var CustomLink = joint.dia.Link.define('examples.CustomLink', {
       defaultLabel: {
         attrs: { text: { text: '*' } }
       }
     });
-
-    paperScroller.$el.css({ width: '100%', height: '100%' }).appendTo('#paper-container');
-    paperScroller.zoom(-0.2);
-    paperScroller.centerContent();
-    let prevAngle = 0;
-    let prevLength = 0;
-    let preY = 0;
-
     for(let i = 0; i < this.split.length; i++){
       let angle = parseInt(this.split[i].substr(0, this.split[i].indexOf('L')));
       let length = parseInt(this.split[i].substring(this.split[i].indexOf('L') + 1));
       let link;
       if(i == 0){
+        angle = angle * - 1;
         link = new CustomLink({
           source: { x: 0, y: 0},
           target: { x: length, y: angle}
         });
         prevLength = length;
+        prevAngle = angle;
+        preY = angle;
       }
       else{
-        var x2 = prevLength + Math.cos(3.14 * (prevAngle - angle)/180) * length;
-        var y2 = prevAngle - Math.sin(3.14 * (angle)/180) * length;
-        x2 = Math.cos((angle+prevAngle)*Math.PI/180) * length + prevLength;
-        y2 = Math.sin((angle+prevAngle)*Math.PI/180) * length + preY;
-
+        angle = angle * - 1;
+        var x2 = Math.cos((angle+prevAngle)*Math.PI/180) * length + prevLength;
+        var y2 = Math.sin((angle+prevAngle)*Math.PI/180) * length + preY;
         console.log('-----------------');
         console.log("X2: " +x2);
         console.log("Y2: " +y2);
@@ -114,8 +144,7 @@ export class LineBuilderComponent implements OnInit {
       //   '.marker-source': { fill: '#31d0c6', stroke: 'none', d: 'M 10 0 L 0 5 L 10 10 z' },
       //   '.marker-target': { fill: '#fe854f', stroke: '#7c68fc', d: 'M 10 0 L 0 5 L 10 10 z' }
       // });
-      this.graph.addCell([link]);
+      graph.addCell([link]);
     }
-
   }
   }
